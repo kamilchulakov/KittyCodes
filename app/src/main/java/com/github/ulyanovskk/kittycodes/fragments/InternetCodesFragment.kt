410f9742ea8b1cb6d4de74e71db2f9344ec4b7fd
ulyanovskk <mr1english4@gmail.com>
2021-08-24 14:37:51 +0300

    No edit text changes are observed

--- app/src/main/java/com/github/ulyanovskk/kittycodes/fragments/InternetCodesFragment.kt
+++ app/src/main/java/com/github/ulyanovskk/kittycodes/fragments/InternetCodesFragment.kt
@@ -1,14 +1,30 @@
 package com.github.ulyanovskk.kittycodes.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.ImageView
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import coil.load
 import com.github.ulyanovskk.kittycodes.viewmodels.InternetCodesViewModel
 import com.github.ulyanovskk.kittycodes.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 class InternetCodesFragment : Fragment() {
 
@@ -16,7 +32,9 @@ class InternetCodesFragment : Fragment() {
         fun newInstance() = InternetCodesFragment()
     }
 
-    private lateinit var viewModel: InternetCodesViewModel
+    private val viewModel: InternetCodesViewModel by viewModels()
+    private lateinit var imageView: ImageView
+    private lateinit var editText: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,13 +45,34 @@ class InternetCodesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val imageView = requireView().findViewById<ImageView>(R.id.catImage)
-        imageView.load(
-            "https://http.cat/404.jpg"
-        ) {
-            crossfade(true)
-            placeholder(R.drawable.ic_launcher_background)
+        imageView = requireView().findViewById(R.id.catImage)
+        editText = requireView().findViewById(R.id.editText)
+        changeCat(viewModel.catUrl.value)
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(
+                Lifecycle.State.STARTED
+            ) {
+                viewModel.catUrl
+                    .onEach { url ->
+                        changeCat(url)
+                    }
+            }
         }
+
+        editText.doOnTextChanged { text, _, _, count ->
+            viewModel.setCatUrl(text?.toString().orEmpty())
+            Log.d("PROCESSED", "CHANGE: " + viewModel.catUrl.value)
+        }
+    }
+
+    private fun changeCat(url: String) {
+        Log.d("PROCESSED", "LOADING")
+        imageView.load(
+                    url
+                ) {
+                    crossfade(true)
+                }
     }
 
 }
--- app/src/main/java/com/github/ulyanovskk/kittycodes/viewmodels/InternetCodesViewModel.kt
+++ app/src/main/java/com/github/ulyanovskk/kittycodes/viewmodels/InternetCodesViewModel.kt
@@ -1,7 +1,16 @@
 package com.github.ulyanovskk.kittycodes.viewmodels
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
-class InternetCodesViewModel : ViewModel() {
+class InternetCodesViewModel() : ViewModel() {
+    private val _catUrl = MutableStateFlow("https://http.cat/404.jpg")
+    val catUrl: StateFlow<String> = _catUrl
+    private val basicUrl: String = "https://http.cat/"
+    private val imageType = ".jpg"
 
+    fun setCatUrl(code: String) {
+        _catUrl.value = basicUrl + code + imageType
+    }
 }
