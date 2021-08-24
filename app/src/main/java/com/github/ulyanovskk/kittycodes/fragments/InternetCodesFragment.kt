bc0cb1b7d763954ef8e824af563ecf528bd0020f
ulyanovskk <mr1english4@gmail.com>
2021-08-24 15:01:06 +0300

    Fix collect instead of onEach

--- app/src/main/java/com/github/ulyanovskk/kittycodes/fragments/InternetCodesFragment.kt
+++ app/src/main/java/com/github/ulyanovskk/kittycodes/fragments/InternetCodesFragment.kt
@@ -10,21 +10,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageView
-import androidx.core.widget.addTextChangedListener
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import coil.load
 import com.github.ulyanovskk.kittycodes.viewmodels.InternetCodesViewModel
 import com.github.ulyanovskk.kittycodes.R
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.lang.Exception
 
 class InternetCodesFragment : Fragment() {
 
@@ -47,22 +46,20 @@ class InternetCodesFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         imageView = requireView().findViewById(R.id.catImage)
         editText = requireView().findViewById(R.id.editText)
-        changeCat(viewModel.catUrl.value)
+        editText.doOnTextChanged { text, _, _, _ ->
+            viewModel.setCatUrl(text?.toString().orEmpty())
+            Log.d("PROCESSED", "CHANGE: " + viewModel.catUrl.value)
+        }
 
         lifecycleScope.launch {
             lifecycle.repeatOnLifecycle(
                 Lifecycle.State.STARTED
             ) {
-                viewModel.catUrl
-                    .onEach { url ->
-                        changeCat(url)
-                    }
+                Log.d("PROCESSED", "IN LAUNCH")
+                viewModel.catUrl.debounce(500).collect {
+                    url -> changeCat(url)
+                }
             }
-        }
-
-        editText.doOnTextChanged { text, _, _, count ->
-            viewModel.setCatUrl(text?.toString().orEmpty())
-            Log.d("PROCESSED", "CHANGE: " + viewModel.catUrl.value)
         }
     }
 
